e0b6aec908468befe92e5ba3fdf063c4917f6d13
LuisE Cruz <Luis.cruzlc01@gmail.com>
2025-12-23 11:52:19 +0100

    generato un ciclo for che inietta le email nell'email generate dal APi

---
+++ script/script.js
@@ -0,0 +1,28 @@
+ const endpoint = "https://flynn.boolean.careers/exercises/api/random/mail";
+
+// Selezione gli elementi
+
+const mails = document.getElementById("mails")
+const generator = document.getElementById("gen-mail")
+
+// Genero le email nella mia lista tramite il buttone
+
+generator.addEventListener("click", () => {
+    mails.innerText = ""
+
+    for (let index = 0; index < 10; index++) {
+        axios.get(endpoint).then((response) => {
+            const result = response.data.response;
+            mails.innerHTML += `
+            <li class="list-unstyled my-3">
+
+                        <div class="bg-primary bg-opacity-10 border-primary rounded text-primary">
+                            ${result}
+                        </div>
+
+                    </li>
+            `;
+        });
+        
+    }
+});
